1214bacb3ef67dcf9c2afbdd3be29318fbe71ea0
June Wunder <june@junewunder.com>
2021-11-22 22:40:02 -0500

    good first upload point

--- src/polar.js
+++ src/polar.js
@@ -11,13 +11,11 @@ import * as PIXI from 'pixi.js'
 import '@pixi/graphics-extras'
 
 const modes = {
-  singleTri: false,
   singleHex: false,
-  unmasked: false,
+  masked: true,
 }
 
 const EMOJI_SIZE = 72
-// const NUM_GLITTER = 200
 const NUM_GLITTER = 1000
 const GLITTER_AREA_SIZE = EMOJI_SIZE * 10
 const GLITTER_VIEW_SIZE = EMOJI_SIZE * 3
@@ -45,17 +43,17 @@ document.body.appendChild(app.view);
 emojis.map(e => app.loader.add(e.name, e.tex))
 app.loader.load(() => main(app))
 
-const makeTriangle = (x, y, sideLen) => ([
+const makeTriangle = (x, y, sideLen, theta) => ([
   x, y + 1,
-  x + cos(PI / 3 * 2) * sideLen - 3,
-  y - sin(PI / 3 * 2) * sideLen - 3,
-  x + cos(PI / 3) * sideLen + 3,
-  y - sin(PI / 3) * sideLen - 3,
+  x + cos(PI / 3 * 2 + theta) * sideLen - 3,
+  y - sin(PI / 3 * 2 + theta) * sideLen - 3,
+  x + cos(PI / 3 + theta) * sideLen + 3,
+  y - sin(PI / 3 + theta) * sideLen - 3,
 ]);
 
-const drawMask = (g, x, y, sideLen) =>
+const drawMask = (g, x, y, sideLen, theta) =>
   g.beginFill(0, 0xffffff)
-    .drawPolygon(makeTriangle(x, y, sideLen))
+    .drawPolygon(makeTriangle(x, y, sideLen, theta))
     .endFill();
 
 class Hexagon {
@@ -65,80 +63,76 @@ class Hexagon {
   tris = [] // PIXI.Sprite[6]
   texture = null // PIXI.Texture
   container = new PIXI.Container()
-  localX = 0
-  localY = 0
-  localTheta = 0
-  localRadius = 0
+  theta = 0
+  radius = 0
+  globalRotation = []
   constructor(bg, bgTex) {
     this.bg = bg
     this.bgTex = bgTex
     this.bgMask = new PIXI.Graphics()
-    if (!modes.unmasked) {
+    if (modes.masked) {
       this.bg.mask = this.bgMask
     } else {
       this.bg.addChild(this.bgMask)
     }
     this.texture = PIXI.RenderTexture.create({
       width: GLITTER_VIEW_SIZE * 3,
-      height: GLITTER_VIEW_SIZE * 3,
+      height: GLITTER_AREA_SIZE * 3,
     })
     this.container.x = this.texture.width / 2
     this.container.y = this.texture.height / 2
-    let maxTris = modes.singleTri ? 1 : 6
-    for (let i = 0; i < maxTris; i++) {
+    for (let i = 0; i < 6; i++) {
     // for (let i = 0; i < 1; i++) {
-    // for (let i = 2; i < 3; i++) {
       const tri = new PIXI.Sprite(bgTex)
       tri.position.set(0, 0)
       if (i % 2 === 0) {
-        tri.rotation = PI / 3 * i
+        this.globalRotation[i] = tri.rotation = PI / 3 * i
       } else {
-        tri.rotation = -(PI / 3 * i)
+        this.globalRotation[i] = tri.rotation = -(PI / 3 * i)
         tri.scale.x *= -1
       }
       this.container.addChild(tri)
       this.tris.push(tri)
     }
-    this.moveTo(this.bg.width / 2, this.bg.height / 2)
+    this.moveTo(PI / 2, this.bg.height / 2)
     app.renderer.render(this.bg, { renderTexture: this.bgTex })
     app.renderer.render(this.container, { renderTexture: this.texture })
   }
 
-  checkBounds(x, y) {
-    // console.log(
-    //   makeTriangle(x, y, GLITTER_VIEW_SIZE),
-    //   this.bgTex.width,
-    //   this.bgTex.height)
-    const x0 = x + cos(PI / 3 * 2) * GLITTER_VIEW_SIZE - 3
-    const y0 = y - sin(PI / 3 * 2) * GLITTER_VIEW_SIZE - 3
-    const x1 = x + cos(PI / 3) * GLITTER_VIEW_SIZE + 3
-    const y1 = y + 1
+  checkBounds(theta, radius) {
+    const [x, y] = this.toXY(theta, radius)
+    const triangle = makeTriangle(x, y, GLITTER_VIEW_SIZE, this.theta)
     return (
-      x0 >= 0 && y0 >= 0 &&
-      x1 <= this.bgTex.width &&
-      y1 <= this.bgTex.height
+      triangle[3] >= 0 &&
+      triangle[4] >= 0 &&
+      triangle[5] <= this.bgTex.width &&
+      triangle[2] <= this.bgTex.height
     )
   }
 
-  moveToPolar(theta, radius) {
-    this.moveTo(
+  toXY(theta, radius) {
+    return [
       this.bgTex.width / 2 + radius * cos(theta),
       this.bgTex.height - radius * sin(theta)
-    )
+    ]
+    // return [atan(y / x), sqrt(x * x + y * y)]
   }
 
-  moveTo(x, y) {
-    if (x === this.localX && y === this.localY) return
+  moveTo(theta, radius) {
+    if (theta === this.theta && radius === this.radius) return
+    this.theta = theta
+    this.radius = radius
+    let [x, y] = this.toXY(theta, radius)
     if (!this.checkBounds(x, y)) return
-    this.localX = x
-    this.localY = y
-    this.localTheta = atan(y / x)
-    this.localRadius = sqrt(x * x + y * y)
-    for (let tri of this.tris) {
-      tri.anchor.set(x / this.bgTex.width, y / this.bgTex.height)
+    // this.localTheta = atan(y / x)
+    // this.localRadius = sqrt(x * x + y * y)
+    // for (let tri of this.tris) {
+    for (let i = 0; i < this.tris.length; i++) {
+      this.tris[i].anchor.set(x / this.bgTex.width, y / this.bgTex.height)
+      this.tris[i].rotation = this.globalRotation[i] + theta
     }
     this.bgMask.clear()
-    drawMask(this.bgMask, x, y, GLITTER_VIEW_SIZE)
+    drawMask(this.bgMask, x, y, GLITTER_VIEW_SIZE, +theta)
     app.renderer.render(this.bg, { renderTexture: this.bgTex })
     app.renderer.render(this.container, { renderTexture: this.texture })
   }
@@ -151,16 +145,13 @@ function main () {
   glitterContainer = new PIXI.Container();
   glitterTex = PIXI.RenderTexture.create({
     width: GLITTER_AREA_SIZE,
-    height: GLITTER_AREA_SIZE * 5,
+    height: GLITTER_AREA_SIZE,
   })
 
   for (let index = 0; index < NUM_GLITTER; index++) {
-    let sprite = PIXI.Sprite.from(choice(emojis).name, {
-      width: EMOJI_SIZE,
-      height: EMOJI_SIZE,
-    });
-    // sprite.width = EMOJI_SIZE
-    // sprite.height = EMOJI_SIZE
+    let sprite = PIXI.Sprite.from(choice(emojis).name);
+    sprite.width = EMOJI_SIZE
+    sprite.height = EMOJI_SIZE
     sprite.anchor.set(0.5)
     sprite.x = random() * glitterTex.width
     sprite.y = random() * glitterTex.height
@@ -169,7 +160,7 @@ function main () {
 
   // make hexagon
   hexagon = new Hexagon(glitterContainer, glitterTex)
-  hexagon.moveToPolar(PI / 2, 100)
+  hexagon.moveTo(PI / 2, 100)
 
   const makeHexSprite = (x, y) => {
     let hexSprite = new PIXI.Sprite(hexagon.texture)
@@ -219,7 +210,7 @@ function onDragEnd(e) {
 function onDragMove(e) {
   let currentX = e.data.global.x
   let currentY = e.data.global.y
-  // console.log(prevX - currentX);
+  console.log(prevX - currentX);
   const dx = prevX - currentX
   const dy = prevY - currentY
   // hexagon.moveToPolar(
@@ -249,7 +240,9 @@ let t = 0
 function mainloop() {
   t += 1
   const x = GLITTER_AREA_SIZE / 2
-  const y = (t % (glitterTex.height - GLITTER_VIEW_SIZE)) + GLITTER_VIEW_SIZE
-  hexagon.moveTo(x, y)
+  // const y = (t % (GLITTER_AREA_SIZE - GLITTER_VIEW_SIZE)) + GLITTER_VIEW_SIZE
+  // const y = t
+  // hexagon.moveTo(x, y)
+  hexagon.moveTo(((t / 100) % PI), 100)
   // hexagon.moveToPolar(PI / 2 + t / 100, 100)
 }
